f8bd32fb01238fa169a94c9a83b353b4f287d90f
sm-remi023 <sm-remi023@student.wethinkcode.co.za>
2024-06-09 14:05:47 +0200

    fixed placement of white container and placed navbar inside white container

--- blog_ui/app/page.tsx
+++ blog_ui/app/page.tsx
@@ -3,11 +3,11 @@ import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
     
-    <div className="min-h-screen bg-transparent bg-gradient-to-tr from-orange-300 to-orange-100 dark:bg-gradient dark:from-blue-700 dark:to-blue-900 backdrop-blur-xl">
-      <NavBar/>
-      <div className="flex justify-center items-center h-full mt-16">
-        <div className="bg-white rounded-lg shadow-lg p-60 w-full max-w-4xl">
-            {/* <h1>Hello</h1> */}
+    <div className="min-h-screen bg-gradient-to-tr from-orange-300 to-orange-100 dark:bg-gradient dark:from-blue-700 dark:to-blue-900">
+      <div className="flex justify-center h-full">
+        <div className="mt-20 bg-white rounded-lg shadow-lg p-60 w-full max-w-4xl">
+          
+          <NavBar/>
         </div>
        </div>
     </div>
--- blog_ui/components/NavBar.tsx
+++ blog_ui/components/NavBar.tsx
@@ -2,14 +2,13 @@ import ThemeToggle from "./ThemeToggle";
 
 const NavBar = () => {
     return (
-        <nav className="py-3 dark:bg-dark">
-            <div className="w-[90%] mx-auto flex justify-between items-center">
-            <h1 className="tetx-3xl font-bold uppercase flex-1 text-gray-900 dark:text-white">
-                Next <span className="text-teal-400">Playground</span>
-            </h1>
-            <div className="flex flex-1 justify-end">
-                <ThemeToggle/>
-            </div>
+        <nav className="py-3 -mt-60">
+            <div className="w-[140%] mx-auto flex justify-between items-center">
+                <h1 className="ml-[-30%] font-bold uppercase flex-1 text-gray-900 dark:text-white">Next <span className="text-teal-400">Playground</span></h1>
+                <div className="flex flex-1 justify-end">
+                    
+                    <ThemeToggle/>
+                </div>
             </div>
         </nav>
     )
